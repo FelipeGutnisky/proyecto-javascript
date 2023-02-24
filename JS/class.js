0fe422363100c91d04acbdaa747c92b130f88172
Felipe Gutnisky <felipegutniskybentes@gmail.com>
2023-02-24 19:46:08 -0300

    Ultimo commit del proyecto

--- JS/class.js
+++ JS/class.js
@@ -12,28 +12,33 @@ class tablas{
     }
 }
 
-const tabla1 = new tablas("1","sunstone","5'2",500,"twin fin","../img/twin-fins/album sunstone 5.2 (2).jpg","../img/twin-fins/album sunstone (3).jpg","../img/twin-fins/album sunstone (4).webp")
-const tabla2 = new tablas("2","darkness","7'3",750,"mid length","../img/mid-length/darkness.webp","../img/mid-length/darkness1.webp","../img/mid-length/darkness2.webp")
-const tabla3 = new tablas("3","lightbender","6'2",950,"twin fin","../img/twin-fins/lightbender.jpg","../img/twin-fins/ligh.jpg","../img/twin-fins/lightbender2.jpg")
-const tabla4 = new tablas("4","twinsman","6'0",630,"twin fin","../img/twin-fins/twinsman.jpg","../img/twin-fins/twinsman1.jpg","../img/twin-fins/twinsman2.jpg")
-const tabla5 = new tablas("5","bandida","5'9",430,"thruster","../img/thruster/surfboards_Bandida.png","../img/thruster/boards_Bandida_bottom.png","")
-const tabla6 = new tablas("6","the medina","5'9",475,"thruster","../img/thruster/Surfboards_TheMedina4.png","../img/thruster/boards_medina_n4.png","")
-const tabla7 = new tablas("7","the game","5'6",480,"thruster","../img/thruster/surfboards_cabianca_TheGaMe.png","../img/thruster/Surfboards_TheGAME.png","")
-const tabla8 = new tablas("8","stance","9'0",950,"single fin","../img/single-fins/single-fin.png","../img/single-fins/single-fin2.png","")
-const tabla9 = new tablas("9","big continental","9'4",1000,"single fin","../img/single-fins/singlefin.png","../img/single-fins/singlefin2.png","")
-const tabla10 = new tablas("10","firewire sunday","7'3",1100,"mid length","../img/mid-length/midfire.png","../img/mid-length/midfire2.png","")
-const tabla11 = new tablas("11","big continental","9'6",1250,"single fin","../img/single-fins/bingcontinental.png","../img/single-fins/bingcontinental2.png","")
-const tabla12 = new tablas("12","channel islands m23","6'6",975,"mid length","../img/mid-length/midlength.png","../img/mid-length/midlength2.png","")
-
-
 //array de objetos:
+
 let productos = []
 
-if(localStorage.getItem("productos")){
-
-    productos = JSON.parse(localStorage.getItem("productos"))
+//setear los productos con una petición async-await
+const cargarProductos = async () => {
+    //ruta relativa del HTML al JSON y abrir con liverServer
+    const response = await fetch("../tablas.json")
+    const data = await response.json()
     
-}else{
-    productos.push(tabla1,tabla2,tabla3,tabla4,tabla5,tabla6,tabla7,tabla8,tabla9,tabla10,tabla11,tabla12)
+    for(let tabla of data){
+        let tablaNueva = new tablas (tabla.id, tabla.modelo, tabla.largo, tabla.precio, tabla.tipo,tabla.imagen1,tabla.imagen2,tabla.imagen3)
+        productos.push(tablaNueva)
+    }
+    
     localStorage.setItem("productos", JSON.stringify(productos))
+}
+
+
+//preguntar si existe algo en el storage con la clave productos
+if(localStorage.getItem("productos")){
+    //cuando no sea la primera vez
+
+    for(let tabla of JSON.parse(localStorage.getItem("productos"))){
+        let tablaNueva = new tablas (tabla.id, tabla.modelo, tabla.largo, tabla.precio, tabla.tipo,tabla.imagen1,tabla.imagen2,tabla.imagen3)
+        productos.push(tablaNueva)
+    }
+}else{
+    cargarProductos()
 }
--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,4 @@
 /* CAPTURAS DEL DOM */
-let twinFinButton = document.getElementById("twinFinButton")
-let singleFinButton = document.getElementById("singleFinButton")
-let midLengthButton = document.getElementById("midLengthButton")
-let thrusterButton = document.getElementById("thrusterButton")
 let tablasDeSurf = document.getElementById("tablasDeSurf")
 let modalBodyCarrito = document.getElementById("modalBodyCarrito")
 let guardarTablaBtn = document.getElementById("guardarTablaBtn")
@@ -12,17 +8,26 @@ let buscador = document.getElementById("buscador")
 let coincidencia = document.getElementById("coincidencia")
 let selectOrden = document.getElementById("selectOrden")
 let selectTipo = document.getElementById("selectTipo")
+let modalCargarTabla = document.getElementById("modalCargarTabla")
+let precioTotal = document.getElementById("precioTotal")
+let botonCarrito = document.getElementById("botonCarrito")
+let botonFinalizarCompra = document.getElementById("botonFinalizarCompra")
 /*FUNCIONES*/
 /* AGREGO MI ARRAY AL HTML*/
 function cargarTablas(array){
-    
+    //Limpio en el DOM el elemento
     tablasDeSurf.innerHTML = ""
-    
+//Para cada objeto de mi array hara lo siguiente    
 for(const tabla of array){
+    //Si el objeto de mi array lleva una imagen en su argumento "imagen3"  genera un carrousel con 3 carousel-item
     if(tabla.imagen3 != ""){
+    //Creo un nuevo elemento en mi DOM
     let nuevaTabla = document.createElement("div")
+    //Le agrego una clase a dicho elemento
     nuevaTabla.classList.add("col-md-6","col-lg-4","col-xl-3","p-2","feat")
+    //Le agrego un style a dicho elemento
     nuevaTabla.style.width = "18rem;"
+    //Le agrego la estructura que va a formar parte de dicho elemento
     nuevaTabla.innerHTML= `
     <div id="${tabla.id}" class="carousel slide">
         <div class="carousel-inner">
@@ -53,14 +58,17 @@ for(const tabla of array){
         <button id= "agregarBtn${tabla.id}" class="btn btn-primary btn-sm">Agregar al carrito</button>
     </div>
 </div>`
+    //Agrego al DOM mediante appendchild dicho elemento a tablasDeSurf capturado anteriormente.
     tablasDeSurf.appendChild(nuevaTabla)
-    
+    //Capturo el boton que me va a agregar los productos al carrito
     let agregarBtn = document.getElementById(`agregarBtn${tabla.id}`)
-    agregarBtn.addEventListener("click", ()=>{
+    //Le adjunto un evento a dicho boton donde al clickearlo me llame dos funciones
+    agregarBtn.addEventListener("click", ()=>{  
         agregarAlCarrito(tabla)
-    })
-    
-    }else if(tabla.imagen3 == ""){
+        //cambia en el icono del carrito el numero que indica la cantidad de productos que se encuentran en el mismo
+        numeroCarrito(productosEnCarrito)
+    })}// Si el objeto de mi array no lleva una imagen en su argumento "imagen3"  genera un carousel con 2 carousel-item
+    else if(tabla.imagen3 == ""){
         let nuevaTabla = document.createElement("div")
         nuevaTabla.classList.add("col-md-6","col-lg-4","col-xl-3","p-2","feat")
         nuevaTabla.style.width = "18rem;"
@@ -96,8 +104,10 @@ for(const tabla of array){
     let agregarBtn = document.getElementById(`agregarBtn${tabla.id}`)
     agregarBtn.addEventListener("click", ()=>{
         agregarAlCarrito(tabla)
+        numeroCarrito(productosEnCarrito)
     })
-}else if(tabla.imagen2 == "" & tabla.imagen3 == ""){
+}//Si el objeto de mi array lleva no una imagen en su argumento "imagen3" y tampoco en su argumento "imagen 2"  genera un carrousel con 1 carousel-item
+else if(tabla.imagen2 == "" & tabla.imagen3 == ""){
     let nuevaTabla = document.createElement("div")
         nuevaTabla.classList.add("col-md-6","col-lg-4","col-xl-3","p-2","feat")
         nuevaTabla.style.width = "18rem;"
@@ -130,90 +140,187 @@ for(const tabla of array){
     let agregarBtn = document.getElementById(`agregarBtn${tabla.id}`)
     agregarBtn.addEventListener("click", ()=>{
         agregarAlCarrito(tabla)
+        numeroCarrito(productosEnCarrito)
     })
 
 }
 }}
 
-
-
+//Llamo a la función para que al cargar la página ya se encuentren cargados los productos
+cargarTablas(productos) 
 
 //CONDICONAL CARRITO
+//Creo una variable para luego almacenar los productos en el carrito  
 let productosEnCarrito
+//Si en el localStorage ya se encuentra la llave carrito
 if(localStorage.getItem("carrito")){
+    //Aplico un JSON.parse para que el texto se almacene en formato JSON
+    //en la variable estructurada "productosEnCarrito"
     productosEnCarrito = JSON.parse(localStorage.getItem("carrito"))
-}else{
+    //establezco en el icono del carrito el numero que indica la cantidad de productos/objetos que se encuentran en la variable
+    numeroCarrito(productosEnCarrito)
+}//Si en el localStorage no se encuentra la llave carrito
+else{
+    //limpio la variable estructurada
     productosEnCarrito = []
+    //Seteo en el localStorage una llave carrito cuyo valor van a ser los productos/objetos que se encuentren en la variable estructurada
     localStorage.setItem("carrito", productosEnCarrito)
+    //establezco en el icono del carrito la cantidad de productos/objetos que se encuentran en la variable cuyo este caso va a ser 0 
+    numeroCarrito(productosEnCarrito)
 }
 
-//AGREGAR AL CARRITO
+// FUNCTION AGREGAR AL CARRITO
 function agregarAlCarrito(tabla){
-    console.log(`La tabla ${tabla.modelo} de largo ${tabla.largo} ha sido agregada al carrito. Vale ${tabla.precio}`)
+    //Creo un variable que almacena un metodo find que me va a buscar si el producto/objeto se encuentran almacenado en la variable estructurada
+    // "productosEnCarrito" mediante su id 
+    let tablaAgregada= productosEnCarrito.find((elem)=>elem.id == tabla.id)
+    //Si no encuentra dicho producto/objeto en la variable estructurada "productosEnCarrito"
+    //la variable "tablaAgregada" tomara el valor de "undefined"
+    if(tablaAgregada == undefined){
+        //Agrego dicho producto/objeto a la variable que almacena los productos en el carrito
+        productosEnCarrito.push(tabla)
+        //Seteo nuevamente el localStorage para que en la llave "carrito" me aparezca el producto/objeto agregado recientemente
+        localStorage.setItem("carrito", JSON.stringify(productosEnCarrito))
     
-    productosEnCarrito.push(tabla)
-    console.log(productosEnCarrito)
-    localStorage.setItem("carrito", JSON.stringify(productosEnCarrito))
+        //Le muestro una alerta al usuario para que sepa que su producto fue agregado al carrito
+        Toastify({
+            text: `La tabla ${tabla.modelo} ${tabla.largo} ha sido AGREGADA al carrito`,
+            duration: 3000,
+            gravity: "top", 
+            position: "right", 
+            style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+            color: "black"
+            },
+        }).showToast();
+    
+
+    }//Si dicho producto ya se encuentra en el carrito
+    else{
+        //Le muestro una alerta para que sepa que su producto ya se encuentra en el carrito
+        Toastify({
+            text: `La tabla ${tabla.modelo} ${tabla.largo} YA ha sido AGREGADA al carrito`,
+            duration: 3000,
+            gravity: "top", 
+            position: "right", 
+            style: {
+            background: "linear-gradient(to right, #500505, #e00a0a)",
+            color: "white"
+            },
+        }).showToast();
+    }
+}
+
+
+    
+
+// FUNCTION CARGAR CARRITO
+function cargarProductosCarrito(array){
+    //limpio el elemento de mi DOM cuyo id es "modalBodyCarrito"
+    modalBodyCarrito.innerHTML = ""
+    //Por cada producto/objeto que se encuentra almacenado dentro del array/variable estructurada "productosEncarrito" modifico el DOM y 
+    //le agrego al elemento cuyo id es "modalBodyCarrito" la siguiente estructura 
+    array.forEach((productosEnCarrito)=>{
+        modalBodyCarrito.innerHTML +=
+        `<div class="card border-primary mb-3" id ="productoCarrito${productosEnCarrito.id}" style="max-width: 540px;">
+                <img class="card-img-top img-fluid "  src="../img/${productosEnCarrito.imagen1}" alt="${productosEnCarrito.modelo}">
+                <div class="card-body">
+                        <h4 class="card-title">${productosEnCarrito.modelo}</h4>    
+                        <p class="card-text">$${productosEnCarrito.precio}</p> 
+                        <button class= "btn btn-danger" id="botonEliminar${productosEnCarrito.id}"><i class="fas fa-trash-alt"></i></button>
+                </div>    
+            </div>
+        `
+})
+    //Segundo forEach para adjuntar evento
+    //Por cada producto/objeto que se encuentra almacenado dentro del array "productosEncarrito"
+    array.forEach((productosEnCarrito) => {
+        //Tomo el boton que sirve para eliminar el producto del carrito mediante su id y le adjunto el siguiente evento
+        document.getElementById(`botonEliminar${productosEnCarrito.id}`).addEventListener("click",()=>{
+        //Tomo el elemento que deseo eliminar mediante su id y lo almaceno dentro de una variable
+        let tablaEnCarrito = document.getElementById(`productoCarrito${productosEnCarrito.id}`)
+        //Remuevo el elemento del DOM 
+        tablaEnCarrito.remove()
+        //Busco el producto/objeto dentro del array "productosEnCarrito" mediante su id y lo almaceno en una nueva variable
+        let tablaEliminar = array.find((tabla) => tabla.id == productosEnCarrito.id)
+        //Creo una nueva variable donde se almacene el indice en el que se encuentre el producto/objeto almacenado en la variable anterior
+        let posicion = array.indexOf(tablaEliminar)
+        //Elimino ese objeto/producto del array 
+        array.splice(posicion,1)
+        //Seteo nuevamente el LocalStorage con el objeto/producto ya eliminado 
+        localStorage.setItem("carrito", JSON.stringify(array))
+        //Calculo nuevamente el total del carrito
+        calcularTotal(array)
+        //establezco nuevamente en el icono del carrito la cantidad de productos que se encuentran en el mismo
+        numeroCarrito(array)
+        })
+})
+//Calculo el total del carrito
+calcularTotal(array)
+//establezco nuevamente en el icono del carrito la cantidad de productos que se encuentran en el mismo
+numeroCarrito(array)
 }
 
 //FUNCTION AGREGAR TABLA
 function agregarTabla(array){
+    //Capturo los input del form 
     let modeloInput = document.getElementById("modeloInput")
     let tamanioInput = document.getElementById("tamanioInput")
     let precioImput = document.getElementById("precioImput")
     let selectQuillas = document.getElementById("selectQuillas")
-    /* let subirImagen = document.getElementById("subirImagen") */
-    
+    //Creo un nuevo objeto/producto mediante los datos ingresados en el input
     const tablaNueva = new tablas(array.length+1, modeloInput.value, tamanioInput.value, parseInt(precioImput.value),selectQuillas.value,"../img/Empty_wave_at_Banzai_Pipeline.jpeg","","")
-    console.log(tablaNueva)
-    //sumarlo a estanteria:
+    //lo sumo al array:
     array.push(tablaNueva)
-    console.log(array)
-    //sumarlo también al Storage:
+    //lo sumo al Storage:
     localStorage.setItem("productos", JSON.stringify(array))
+    //lo sumo al DOM
     cargarTablas(array)
-    //reset input
-    modeloInput.value = ""
-    tamanioInput.value = ""
-    precioImput.value = ""
-    selectQuillas.value = ""
-    
-}
+    //reseteo el input
+    modalCargarTabla.reset()
+} 
 
 //FUNCTION BUSCAR 
 function buscar(buscado, array){
+    //Filtro en el array aquellos objetos donde cuyos argumentos "modelo" o "largo" incluyan alguna letra o número buscado por el usuario
+    //y los almaceno en una variable
     let busquedaArray = array.filter(
         (tabla)=> tabla.modelo.toLowerCase().includes(buscado) || tabla.largo.toLowerCase().includes(buscado)
     )
-    if(busquedaArray.length == 0){
-        coincidencia.innerHTML = `<h3>No hay coincidencias con su búsqueda</h3>`
-        cargarTablas(busquedaArray)
-    }else{
-        coincidencia.innerHTML = ""
-        cargarTablas(busquedaArray)
-    }
+    
+    busquedaArray.length == 0 ?
+    //Si no hay ningun array filtrado en esa busqueda modificio el DOM y agrego el siguiente mensaje "No hay coincidencias con su búsqueda"
+    (coincidencia.innerHTML = `<h3>No hay coincidencias con su búsqueda</h3>`, cargarTablas(busquedaArray)) 
+    ://Si hay algun array filtrado modifico el DOM y cargo el mismo exclusivamente
+    (coincidencia.innerHTML = "", cargarTablas(busquedaArray))
 }
 
 //FUCTION ORDENAR
 function ordenarMenorMayor(array){
-    
+
     const menorMayor = [].concat(array)
+    //ordeno los objetos del array de menor a mayor
     menorMayor.sort((a,b) => a.precio - b.precio)
+    //los cargo en el DOM en ese orden
     cargarTablas(menorMayor)
 }
 function ordenarMayorMenor(array){
     
     const mayorMenor = [].concat(array)
+    //ordeno los objetos del array de mayor a menor
     mayorMenor.sort((param1, param2)=>{
         return param2.precio - param1.precio
     })
+    //los cargo en el DOM en ese orden
     cargarTablas(mayorMenor)
 }
 
 //FUNCTION FILTRAR POR TIPO
 function filtrarTwinFin(array,tipo){
     const twinFin = [].concat(array) 
+    //filtro los objetos del array por el "tipo" y los almaceno en una constante
     const result = twinFin.filter(tabla => tabla.tipo == tipo);
+    //cargo en el DOM aquellos objetos filtrados
     cargarTablas(result)
 }
 function filtrarSingleFin(array,tipo){
@@ -235,17 +342,80 @@ function filtrarThruster(array,tipo){
     
 }
 
+// FUNCTION TOTAL
+function calcularTotal(array){
+    //sumo los argumento "precio" de aquellos objetos del array que se encuentren en el carrito
+    let total = array.reduce((acc, productoCarrito)=>acc + productoCarrito.precio ,0)
+    //si esa suma es igual a 0, modifico el DOM y en el modal del carrito se encuentra este mensaje "No hay productos en el carrito"
+    total == 0 ? precioTotal.innerHTML = `No hay productos en el carrito` :
+    // si esa suma es distinta a 0
+    precioTotal.innerHTML = `El total del carrito es <strong>${total}$USD</strong>`
+}
+//FUNCTION NUMERO CARRITO
+function numeroCarrito(numeroEnCarrito){
+    let carritoNumero = document.getElementById("carritoNumero")
+    //establezo que el largo de mi parametro va a ser la sintaxis del elemento  
+    carritoNumero.innerHTML = numeroEnCarrito.length
+}
+//FUNCTION FINALIZAR COMPRA 
+function finalizarCompra(){
+    //le muestro una alerta al usuario si quiere efectivamente realizar la compra
+    Swal.fire({
+        title: 'Está seguro de realizar la compra)',
+        icon: 'info',
+        showCancelButton: true,
+        confirmButtonText: 'Sí, seguro',
+        cancelButtonText: 'No, no quiero',
+        confirmButtonColor: 'green',
+        cancelButtonColor: 'red',
+    }).then((result)=>{
+        //si el usuario confirma la compra
+        if(result.isConfirmed){
+            //le envío otra alerta
+            Swal.fire({
+                title: 'Compra realizada',
+                icon: 'success',
+                confirmButtonColor: 'green',
+                text: `Muchas gracias por su compra ha adquirido nuestros productos. `,
+                })
+                //vacío la variable estructurada
+                productosEnCarrito = []
+                //visualizo nuevamente la cantidad de productos que se encuentran en el mismo
+                numeroCarrito(productosEnCarrito)
+                //remuevo de mi localStorage la llave "carrito"
+                localStorage.removeItem("carrito")
+        }//si el usuario confirma la compra
+        else{
+            //le envío otra alerta
+            Swal.fire({
+                title: 'Compra no realizada',
+                icon: 'info',
+                text: `La compra no ha sido realizada! ATENCIÓN sus productos siguen en el carrito`,
+                confirmButtonColor: 'green',
+                timer:3500
+            })
+        }
+    }
+    )
+}
+
+
+
 /*EVENTOS*/
 guardarTablaBtn.addEventListener("click", ()=>{
-    agregarTabla(productos)}
-)
+    //agrega la tabla ingresada al localStorage y DOM
+    agregarTabla(productos)
+})
 
+//Visualiza las tablas 
 verTablas.onclick = ()=>{cargarTablas(productos)}
 
+//Oculta las tablas 
 ocultarTablas.onclick = function(){
     tablasDeSurf.innerHTML =""
 }
 
+//Busca las coincidencia con los objetos y los visualiza 
 buscador.addEventListener("input", ()=>{
 buscar(buscador.value, productos)
 })
@@ -274,3 +444,11 @@ if(selectTipo.value == 1){
 }
 })
 
+//Agrega los productos al carrito
+botonCarrito.addEventListener("click", () =>{
+    cargarProductosCarrito(productosEnCarrito)
+})
+
+//Manda las alertas al finalizar la compra
+botonFinalizarCompra.addEventListener("click", ()=>{
+    finalizarCompra()})
